486ecb5c3aef1128f5bcc5900b2dfdad948a3ef4
evandragich <evandragich@gmail.com>
2021-11-26 08:26:20 -0800

    aded cleaning script

---
+++ cleaning_script.R
@@ -0,0 +1,134 @@
+library(tidyverse)
+library(glue)
+library(rvest)
+library(janitor)
+
+# take 2
+# https://library.ndsu.edu/ir/bitstream/handle/10365/25890/Forecasting%20Point%20Spread%20for%20Women’s%20Volleyball.pdf?sequence=1&isAllowed=y
+
+
+
+
+get_set_data <- function(n, url) {
+
+  # https://www.dataquest.io/blog/web-scraping-in-r-rvest/
+  x <- read_html(url) %>%
+    html_element("body") %>%
+    html_element("form") %>%
+    html_element("main") %>%
+    html_element("article") %>%
+    html_element(".sidearm-responsive-tabs.main-tabs") %>%
+    html_element("#play-by-play") %>%
+    html_element(".sidearm-responsive-tabs") %>%
+    html_elements(glue("#set-{n}")) %>%
+    html_element("table") %>%
+    html_table()
+
+  if (length(x) == 0) {
+    NULL
+  } else {
+    x <- x[[1]] %>%
+      clean_names() %>%
+      filter(score != "")
+
+    # https://stackoverflow.com/questions/48211229/reorder-vector-in-r-according-to-index-vector
+    temp <- colnames(x)
+    index <- c(1, 2, 3, 4, 6, 5, 7, 9, 8)
+    colnames(x) <- temp[order(index)]
+
+    x %>%
+      mutate(uw_serve = if_else(serve_team == "UW",
+        1,
+        0
+      )) %>%
+      mutate(opp_serve = if_else(serve_team != "UW",
+        1,
+        0
+      )) %>%
+      rename(uw_score = uw_play_description) %>%
+      rename(opp_score = ends_with("_play_description")) %>%
+      select(-c(scoring_team_indicator, visiting_team_score, scoring_team_logo, home_team_score)) %>%
+      mutate(
+        uw_score = as.numeric(uw_score),
+        opp_score = as.numeric(opp_score),
+        point_diff = uw_score - opp_score,
+        uw_point = if_else(point_diff > lag(point_diff, default = 0), TRUE, FALSE),
+        uw_so_suc = if_else((opp_serve == 1 & uw_point), 1, 0),
+        uw_ps_suc = if_else((uw_serve == 1 & uw_point), 1, 0),
+        uw_so_cum = cumsum(uw_so_suc),
+        uw_ps_cum = cumsum(uw_ps_suc),
+        uw_so_att = cumsum(opp_serve),
+        uw_ps_att = cumsum(uw_serve)
+      ) %>%
+      mutate(
+        uw_so_pct = if_else(uw_so_att != 0,
+          uw_so_cum / uw_so_att,
+          0
+        ),
+        uw_ps_pct = if_else(uw_ps_att != 0,
+          uw_ps_cum / uw_ps_att,
+          0
+        )
+      ) %>%
+      rowwise() %>%
+      mutate(
+        uw_kill = if_else(str_detect(play_description, "Kill by") & uw_point, 1, 0),
+        opp_kill = if_else(str_detect(play_description, "Kill by") & !(uw_point), 1, 0),
+        opp_atk_err = if_else(str_detect(play_description, "Attack error by") & uw_point, 1, 0),
+        uw_atk_err = if_else(str_detect(play_description, "Attack error by") & !(uw_point), 1, 0),
+        uw_blk = if_else(str_detect(play_description, "Block by") & uw_point, 1, 0),
+        opp_blk = if_else(str_detect(play_description, "Block by") & !(uw_point), 1, 0),
+        opp_blk_err = if_else(str_detect(play_description, "Block error") & uw_point, 1, 0),
+        uw_blk_err = if_else(str_detect(play_description, "Block error") & !(uw_point), 1, 0),
+        uw_srv_ace = if_else(str_detect(play_description, "Service ace") & uw_point, 1, 0),
+        opp_srv_ace = if_else(str_detect(play_description, "Service ace") & !(uw_point), 1, 0),
+        opp_srv_err = if_else(str_detect(play_description, "Service err") & uw_point, 1, 0),
+        uw_srv_err = if_else(str_detect(play_description, "Service err") & !(uw_point), 1, 0)
+      ) %>%
+      ungroup() %>%
+      mutate(across(c(uw_kill:uw_srv_err),
+        cumsum,
+        .names = "cum_{.col}"
+      ))
+  }
+}
+
+game_data <- map_dfr(c(1:5),
+  ~ get_set_data(
+    n = .x,
+    url = "https://gohuskies.com/sports/womens-volleyball/stats/2020/arizona-state/boxscore/19499"
+  ),
+  .id = "set"
+)
+
+game_data2 <- map_dfr(c(1:5),
+  ~ get_set_data(
+    n = .x,
+    url = "https://gohuskies.com/sports/womens-volleyball/stats/2020/arizona/boxscore/19493"
+  ),
+  .id = "set"
+)
+
+get_match_data <- function(url) {
+  x <- map_dfr(c(1:5),
+               ~ get_set_data(
+                 n = .x,
+                 url = url
+               ),
+               .id = "set"
+  ) %>%
+    group_by(set) %>%
+    mutate(uw_set_win = if_else(max(uw_score) > max(opp_score), 1, 0),
+           opp_set_win = if_else(max(uw_score) < max(opp_score), 1, 0)) %>%
+    group_by(set) %>%
+    mutate(across(c(uw_set_win, opp_set_win),
+                  cumsum,
+                  .names = "cum_{.col}"
+    ))
+  
+  x
+}
+  
+y <- get_match_data(url)
+  
+  
